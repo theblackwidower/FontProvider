98603d370123eb7e039819219d0f6534d5b8d98d
T Duke Perry <theblackwidower@noprestige.com>
2018-12-07 04:10:07 -0500

    Removed crashlytics components
    Testing if the removal of crashlytics and fabric.io would impair the app's functionality.

--- app/src/main/java/moe/shizuku/fontprovider/FontProviderApplication.java
+++ app/src/main/java/moe/shizuku/fontprovider/FontProviderApplication.java
@@ -3,10 +3,6 @@ package moe.shizuku.fontprovider;
 import android.app.Application;
 import android.content.Context;
 
-import com.crashlytics.android.Crashlytics;
-import com.crashlytics.android.core.CrashlyticsCore;
-
-import io.fabric.sdk.android.Fabric;
 import moe.shizuku.fontprovider.font.FontManager;
 
 /**
@@ -23,12 +19,6 @@ public class FontProviderApplication extends Application {
         }
 
         sInitialized = true;
-
-        Crashlytics crashlyticsKit = new Crashlytics.Builder()
-                .core(new CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build())
-                .build();
-
-        Fabric.with(context, crashlyticsKit);
 
         FontProviderSettings.init(context);
         FontManager.init(context);
